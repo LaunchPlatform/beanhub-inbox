4384211fad6d5100bc9bbf0ae1ece66d8ccb1d7f
Fang-Pen Lin <hello@fangpenlin.com>
2025-04-12 01:22:27 -0700

    Add test extract

--- tests/test_llm.py
+++ tests/test_llm.py
@@ -223,3 +223,113 @@ def test_build_archive_attachment_model(
         output_folders=output_folders, attachment_count=attachment_count
     )
     assert model.model_json_schema() == expected.model_json_schema()
+
+
+# TODO: temporary
+def test_extract():
+    from transformers import AutoTokenizer
+
+    model_name = "Qwen/Qwen2.5-14B-Instruct-1M"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    resp_model_cls = build_response_model(
+        output_columns=[
+            OutputColumn(
+                name="desc",
+                type=OutputColumnType.str,
+                description="summary of the transaction",
+            ),
+            OutputColumn(
+                name="amount",
+                type=OutputColumnType.decimal,
+                description="transaction amount",
+            ),
+            OutputColumn(
+                name="txn_id",
+                type=OutputColumnType.str,
+                description="Id of transaction",
+            ),
+            OutputColumn(
+                name="date",
+                type=OutputColumnType.date,
+                description="transaction date",
+            ),
+        ],
+        output_folders=["receipts", "invoices"],
+        attachment_count=2,
+    )
+    import json
+
+    print(json.dumps(resp_model_cls.model_json_schema()))
+    # return
+    prompt = textwrap.dedent(f"""\
+    Extract data from the following email into JSON payload.
+    Output ArchiveAttachment in the JSON for attachment files that look like invoices to the "invoices" folder.
+    Output ArchiveAttachment in the JSON for attachment files that look like receipts to the "receipts" folder.
+    
+    # JSON schema
+    
+    ```
+    {json.dumps(resp_model_cls.model_json_schema())}
+    ```
+    
+    # Output folders
+    
+    - receipts
+    - invoices
+    
+    # Attachments
+    
+    1. filename="GitHub-invoice.pdf", mime_type="application/pdf"
+    2. filename="GitHub-receipt.pdf", mime_type="application/pdf"
+    
+    # Email content
+    
+    We received payment for your GitHub.com subscription. Thanks for your business!
+
+    Questions? Visit https://github.com/contact
+
+    ------------------------------------
+    GITHUB RECEIPT - PERSONAL SUBSCRIPTION - fangpenlin
+
+
+    GitHub Developer Plan - Month: $4.00 USD
+    Apr 1, 2025 - Apr 30, 2025
+
+
+    Tax: $0.00 USD
+    Total: $4.00 USD*
+
+    Charged to: Visa (4*** **** **** 1234)
+    Transaction ID: 6cd4f0a6-0686-45bf-a077-775e95206da6
+    Date: 01 Apr 2025 09:55AM PDT
+
+
+    GitHub, Inc.
+    88 Colin P. Kelly Jr. Street
+    San Francisco, CA 94107
+    ------------------------------------
+
+    * VAT/GST paid directly by GitHub, where applicable
+    """)
+
+    # print(tokenizer.apply_chat_template([{"role": "user", "content": prompt}], tokenize=False, add_generation_prompt=True))
+    from outlines import models, generate
+
+    model = models.transformers(
+        model_name,
+        device="auto",
+        model_kwargs=dict(
+            torch_dtype="auto",
+            device_map="auto",
+        ),
+    )
+    generator = generate.json(model, resp_model_cls)
+    result = generator(
+        tokenizer.apply_chat_template(
+            [{"role": "user", "content": prompt}],
+            tokenize=False,
+            add_generation_prompt=True,
+        )
+    )
+    print(result)
