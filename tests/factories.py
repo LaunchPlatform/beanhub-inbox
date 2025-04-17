7b1f5a8e39cd6361da5474465bdfdc029099c658
Fang-Pen Lin <hello@fangpenlin.com>
2025-04-17 15:50:54 -0700

    Add factories

--- tests/factories.py
+++ tests/factories.py
@@ -1,6 +1,11 @@
+import dataclasses
+
+from factory import Dict
 from factory import Factory
 from factory import Faker
 from factory import LazyFunction
+from factory import List
+from factory import SubFactory
 from faker import Faker as OriginalFaker
 
 fake = OriginalFaker()
@@ -8,13 +13,51 @@ fake = OriginalFaker()
 from beanhub_inbox.data_types import InboxEmail
 
 
+@dataclasses.dataclass(frozen=True)
+class EmailAttachment:
+    content: bytes
+    mime_type: str = "application/octet-stream"
+    filename: str | None = None
+
+
+@dataclasses.dataclass(frozen=True)
+class EmailFile:
+    headers: dict[str, str]
+    subject: str
+    from_addresses: list[str]
+    recipients: list[str]
+    tags: list[str] | None = None
+    attachments: list[EmailAttachment] | None = None
+
+
 class InboxEmailFactory(Factory):
     id = Faker("uuid4")
-    message_id = Faker("slug")
-    subject = Faker("sentence")
-    from_addresses = LazyFunction(lambda: [fake.email()])
-    recipients = LazyFunction(lambda: [fake.email()])
-    headers = LazyFunction(lambda: {fake.slug(): fake.slug()})
+    mime = Faker("slug")
 
     class Meta:
         model = InboxEmail
+
+
+class EmailAttachmentFactory(Factory):
+    content = Faker("paragraph")
+    mime_type = Faker("mime_type")
+    filename = None
+
+    class Meta:
+        model = EmailAttachment
+
+
+class EmailFileFactory(Factory):
+    subject = Faker("sentence")
+    from_addresses = LazyFunction(lambda: [fake.email()])
+    recipients = LazyFunction(lambda: [fake.email()])
+    headers = Dict(
+        {
+            "Date": Faker("past_datetime"),
+        }
+    )
+    attachments = List([SubFactory(EmailAttachmentFactory, mime_type="text/plain")])
+    tags = None
+
+    class Meta:
+        model = EmailFile
