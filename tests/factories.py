9a4089cebc58b3f4c1ac5f402ac990ab4e1a7c95
Fang-Pen Lin <hello@fangpenlin.com>
2025-04-17 17:48:02 -0700

    Add make_email_msg

--- tests/factories.py
+++ tests/factories.py
@@ -1,4 +1,5 @@
 import dataclasses
+from email.message import EmailMessage
 
 from factory import Dict
 from factory import Factory
@@ -27,6 +28,8 @@ class EmailFile:
     from_addresses: list[str]
     recipients: list[str]
     tags: list[str] | None = None
+    text: EmailAttachment | None = None
+    html: EmailAttachment | None = None
     attachments: list[EmailAttachment] | None = None
 
 
@@ -39,7 +42,7 @@ class InboxEmailFactory(Factory):
 
 
 class EmailAttachmentFactory(Factory):
-    content = Faker("paragraph")
+    content = LazyFunction(lambda: fake.paragraph().encode("utf8"))
     mime_type = Faker("mime_type")
     filename = None
 
@@ -56,8 +59,45 @@ class EmailFileFactory(Factory):
             "Date": Faker("past_datetime"),
         }
     )
-    attachments = List([SubFactory(EmailAttachmentFactory, mime_type="text/plain")])
+    text = SubFactory(EmailAttachmentFactory, mime_type="text/plain")
+    html = SubFactory(EmailAttachmentFactory, mime_type="text/html")
+    attachments = None
     tags = None
 
     class Meta:
         model = EmailFile
+
+
+def make_email_msg(email_file: EmailFile) -> EmailMessage:
+    msg = EmailMessage()
+    msg["From"] = ", ".join(email_file.from_addresses)
+    msg["To"] = ", ".join(email_file.recipients)
+    msg["Subject"] = email_file.subject
+
+    content_parts = []
+    if email_file.text is not None:
+        content_parts.append(email_file.text)
+    if email_file.html is not None:
+        content_parts.append(email_file.html)
+    if not content_parts:
+        raise ValueError("Need to set at least one of text or html")
+
+    for i, part in enumerate(content_parts):
+        if i == 0:
+            method = msg.set_content
+        else:
+            method = msg.add_alternative
+        main_type, sub_type = part.mime_type.split("/")
+        method(part.content, maintype=main_type, subtype=sub_type)
+
+    if email_file.attachments is not None:
+        for attachment in email_file.attachments:
+            main_type, sub_type = attachment.mime_type.split("/")
+            msg.add_attachment(
+                b"xxx", maintype="application", subtype="pdf", filename="foobar"
+            )
+    # for attachment in email_file.attachments:
+    #     msg.add_attachment(
+    #         file_data, maintype=attachment.mime_type, subtype=subtype, filename=file_name
+    #     )
+    return msg
